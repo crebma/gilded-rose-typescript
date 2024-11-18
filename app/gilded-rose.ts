ae09c8295427024cc95f22b85a5b0ae2207a8ca5
Amber Conville <crebma@gmail.com>
2024-11-18 11:15:42 -0500

    chore: change all double equals to triple equals

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,14 +21,14 @@ export class GildedRose {
     this.items.forEach((item) => {
       if (item.name === 'Sulfuras, Hand of Ragnaros') return;
 
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
           item.quality = item.quality - 1
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
               if (item.quality < 50) {
                 item.quality = item.quality + 1
@@ -46,8 +46,8 @@ export class GildedRose {
       item.sellIn = item.sellIn - 1;
 
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.name !== 'Aged Brie') {
+          if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
               item.quality = item.quality - 1
             }
