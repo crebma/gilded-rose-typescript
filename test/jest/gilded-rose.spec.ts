e51e9cba7b4911ce654272ca3dddea8bbe285123
Amber Conville <crebma@gmail.com>
2024-11-18 11:11:20 -0500

    tests: add ignored failing test for conjured item

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,11 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import {Item, GildedRose} from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  xdescribe('Conjured Items', () => {
+    it('should degrade twice as fast as regular items', () => {
+      const gildedRose = new GildedRose([new Item("Conjured Mana Cake", 3, 6)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(4);
+    });
   });
 });
