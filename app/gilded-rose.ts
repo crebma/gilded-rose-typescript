c8f8f7015e8445ac4ae7dc8250b3f19655998194
Amber Conville <crebma@gmail.com>
2024-11-18 11:23:19 -0500

    refactor: pull out helper methods for quality and sellIn modification

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,6 +10,18 @@ export class Item {
   }
 }
 
+function decreaseQuality(item: Item) {
+  item.quality = item.quality - 1
+}
+
+function decreaseSellIn(item: Item) {
+  item.sellIn = item.sellIn - 1;
+}
+
+function increaseQuality(item: Item) {
+  item.quality = item.quality + 1
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -23,41 +35,41 @@ export class GildedRose {
 
       if (item.name === 'Aged Brie') {
         if (item.quality < 50) {
-          item.quality = item.quality + 1
+          increaseQuality(item);
         }
       } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality < 50) {
-          item.quality = item.quality + 1
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+          increaseQuality(item);
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              increaseQuality(item);
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              increaseQuality(item);
             }
+          }
         }
       } else {
         if (item.quality > 0) {
-          item.quality = item.quality - 1
+          decreaseQuality(item);
         }
       }
 
-      item.sellIn = item.sellIn - 1;
+      decreaseSellIn(item);
 
       if (item.sellIn < 0) {
         if (item.name === 'Aged Brie') {
           if (item.quality < 50) {
-            item.quality = item.quality + 1
+            increaseQuality(item);
           }
         } else {
           if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
             item.quality = item.quality - item.quality
           } else {
             if (item.quality > 0) {
-              item.quality = item.quality - 1
+              decreaseQuality(item)
             }
           }
         }
